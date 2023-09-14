8ed985d207791d50e151f7514a7867401faf843f
MakarivIvanDev <Biomoloko1@gmail.com>
2023-09-14 14:02:35 +0300

    from note

--- app/src/main/java/com/example/securitiesapp/MainActivity.kt
+++ app/src/main/java/com/example/securitiesapp/MainActivity.kt
@@ -2,20 +2,95 @@ package com.example.securitiesapp
 
 import android.app.DatePickerDialog
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import android.text.Editable
+import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.graphics.red
 import com.example.securitiesapp.databinding.ActivityMainBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class MainActivity : AppCompatActivity() {
     lateinit var bindingClass: ActivityMainBinding
     private lateinit var datePickerDialog : DatePickerDialog
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         val myError : String = "Ошибка!"
+
+        val defDaM = 10
+        val defY = 2020
+        var startDate = "$defDaM.$defDaM.$defY"
+        var endDate = "$defDaM.$defDaM.$defY"
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+        var parsedStartDate = LocalDate.parse(startDate, formatter)
+        var parsedEndDate = LocalDate.parse(endDate, formatter)
+        var daysBetween : Long = 0
 
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+
+
+    //Calendars invisibility
+        bindingClass.StartCalendar.visibility = View.GONE
+        bindingClass.EndCalendar.visibility = View.GONE
+
+
+    //Start Date works----
+        bindingClass.StartDateButton.setOnClickListener{
+
+            if(bindingClass.StartCalendar.visibility == View.GONE) {
+                bindingClass.StartCalendar.visibility = View.VISIBLE
+                bindingClass.EndCalendar.visibility = View.GONE
+            } else {
+                bindingClass.StartCalendar.visibility = View.GONE
+            }
+        }
+        
+        bindingClass.StartCalendar.setOnDateChangeListener { calendarView, i, i2, i3 ->
+            bindingClass.StartDateButton.text = ProjectFunctions.MyCalendarDateFormat(i, i2, i3)
+            startDate = ProjectFunctions.MyCalendarDateFormat(i, i2, i3)
+
+            parsedStartDate = LocalDate.parse(startDate, formatter)
+            daysBetween = ChronoUnit.DAYS.between(parsedStartDate, parsedEndDate) + 1
+
+            bindingClass.CalculatedSellTime.setText(
+                daysBetween.toString()
+            )
+        }
+    //end----
+
+    //End Date works----
+        bindingClass.EndDateButton.setOnClickListener{
+            parsedEndDate = LocalDate.parse(endDate, formatter)
+
+            if (bindingClass.EndCalendar.visibility == View.GONE) {
+                bindingClass.EndCalendar.visibility = View.VISIBLE
+                bindingClass.StartCalendar.visibility = View.GONE
+            } else {
+                bindingClass.EndCalendar.visibility = View.GONE
+            }
+        }
+
+        bindingClass.EndCalendar.setOnDateChangeListener { calendarView, i, i2, i3 ->
+            bindingClass.EndDateButton.text = ProjectFunctions.MyCalendarDateFormat(i, i2, i3)
+            endDate = ProjectFunctions.MyCalendarDateFormat(i, i2, i3)
+
+            parsedEndDate = LocalDate.parse(endDate, formatter)
+            daysBetween = ChronoUnit.DAYS.between(parsedStartDate, parsedEndDate)
+
+            bindingClass.CalculatedSellTime.setText(
+                daysBetween.toString()
+            )
+        }
+    //end----
 
 
         //Calculations Button
@@ -46,6 +121,7 @@ class MainActivity : AppCompatActivity() {
             val nKD = ProjectFunctions.GetParseFloat(bindingClass.NKD.text.toString(), myError)
             bindingClass.NKD.setText(nKD.toString())
 
+
             //установка значений в нижние поля
             bindingClass.DirtyPrice.text = ProjectFunctions.DirtyPriceCalc(cleanPrice, nKD).toString()
 
@@ -53,10 +129,23 @@ class MainActivity : AppCompatActivity() {
 
             bindingClass.CurrentIncome.text = ProjectFunctions.CurrentIncomeCalc(paymentsAtYear, couponSize, cleanPrice).toString()
 
+            bindingClass.SimpleIncome.text = ProjectFunctions.SimpleIncomeCalc(ProjectFunctions.DirtyPriceCalc(cleanPrice, nKD)
+                ,daysBetween.toFloat(),couponSize,nominal,allPayments).toString()
+
+            bindingClass.TaxIncome.text = ProjectFunctions.TaxIncomeCalc(ProjectFunctions.DirtyPriceCalc(cleanPrice, nKD),
+                        daysBetween.toFloat(),couponSize,nominal,allPayments).toString()
+
+
+            bindingClass.DirtyPrice.text = ProjectFunctions.IsNanOrInfin(bindingClass.DirtyPrice.text.toString())
+            bindingClass.CouponIncome.text = ProjectFunctions.IsNanOrInfin(bindingClass.CouponIncome.text.toString())
+            bindingClass.CurrentIncome.text = ProjectFunctions.IsNanOrInfin(bindingClass.CurrentIncome.text.toString())
+            bindingClass.SimpleIncome.text = ProjectFunctions.IsNanOrInfin(bindingClass.SimpleIncome.text.toString())
+            bindingClass.TaxIncome.text = ProjectFunctions.IsNanOrInfin(bindingClass.TaxIncome.text.toString())
         }
 
         //Reset Button
         bindingClass.ResetButton.setOnClickListener(){
+            var defaultDate = "ДД.ММ.ГГГГ"
             bindingClass.NominalPrice.text.clear()
             bindingClass.CouponSize.text.clear()
             bindingClass.PaymentsAtYear.text.clear()
@@ -69,6 +158,9 @@ class MainActivity : AppCompatActivity() {
             bindingClass.CurrentIncome.text = "________"
             bindingClass.SimpleIncome.text = "________"
             bindingClass.TaxIncome.text = "________"
+
+            bindingClass.StartDateButton.text = defaultDate
+            bindingClass.EndDateButton.text = defaultDate
         }
     }
 }
--- app/src/main/java/com/example/securitiesapp/ProjectFunctions.kt
+++ app/src/main/java/com/example/securitiesapp/ProjectFunctions.kt
@@ -1,11 +1,10 @@
 package com.example.securitiesapp
 
-import android.app.DatePickerDialog
-import android.graphics.Color
-import android.widget.DatePicker
-import com.example.securitiesapp.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
 
 object ProjectFunctions {
+
     //Для установки введенного значения в верхнее поле, если не число - ошибка
     fun TextInUpField(number : Float) : String{
         if(number == 0f){
@@ -25,27 +24,46 @@ object ProjectFunctions {
 
     //блок формул рассчета
     fun DirtyPriceCalc(cleanPrice : Float, nkd : Float) : Float{
-        return cleanPrice + nkd
+        return IsNanReturning(cleanPrice + nkd)
     }
 
     fun CouponInkomeCalc(paymentsAtYear : Float, couponSize : Float, nominal : Float) : Float{
-        if (!nominal.isNaN()){
-            return ((paymentsAtYear*couponSize)/nominal) * 100
-        } else {
-            return 0f
-        }
+        return IsNanReturning(((paymentsAtYear*couponSize)/nominal) * 100)
     }
 
     fun CurrentIncomeCalc(paymentsAtYear : Float, couponSize : Float, cleanPrice: Float) : Float{
-        return ((paymentsAtYear*couponSize)/cleanPrice) * 100
+        return IsNanReturning(((paymentsAtYear*couponSize)/cleanPrice) * 100)
+
     }
 
     fun SimpleIncomeCalc(dirtyPrice : Float, daysUntil : Float, couponSize : Float ,nominal : Float, allPayments : Float) : Float{
-        return ((nominal - dirtyPrice + (couponSize * allPayments))/dirtyPrice) * (365/daysUntil) * 100
+        return IsNanReturning(((nominal - dirtyPrice + (couponSize * allPayments))/dirtyPrice) * (365/daysUntil) * 100)
     }
 
     fun TaxIncomeCalc(dirtyPrice : Float, daysUntil : Float, couponSize : Float, nominal : Float, allPayments : Float) : Float {
-        return (((nominal - dirtyPrice + allPayments * couponSize) * 0.87f) / dirtyPrice) * (365 / daysUntil) * 100
+        return IsNanReturning(((((nominal - dirtyPrice + allPayments * couponSize) * 0.87f) / dirtyPrice) * (365 / daysUntil) * 100))
+    }
+    //конец блока формул
+
+
+    fun MyCalendarDateFormat  (i : Int, i2 : Int , i3 : Int) : String{
+        return (if (i3 < 10){"0$i3."} else "$i3.") + (if((i2+1) < 10){"0${i2+1}."} else "${i2+1}.") + "$i"    }
+
+    fun IsNanOrInfin ( checkAnswer : String) : String{
+        if(checkAnswer == "NaN" || checkAnswer == "Infinity" || checkAnswer == "-Infinity")
+        {
+            return "Ошибка!"
+        } else {
+            return checkAnswer
+        }
     }
 
+    //Nan value cannot be rounded so here we check - can we round it or not
+    fun IsNanReturning(result : Float) : Float{
+        if(result.isNaN() == true){
+            return result
+        } else {
+            return ((result * 100.0).roundToInt() / 100.0).toFloat()
+        }
+    }
 }
